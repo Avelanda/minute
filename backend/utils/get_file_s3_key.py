fd23b14f15083d933e5250e5c7e452e7fe4aac10
Avelanda <gorthell@gmail.com>
2025-11-07 16:42:48 +0200

    Update get_file_s3_key.py
    Addressing a granular model by modular properties, and accessing its functional components through performance scaling.

--- backend/utils/get_file_s3_key.py
+++ backend/utils/get_file_s3_key.py
@@ -1,3 +1,6 @@
+# Copyright © 2025 !Avelanda.
+# All rights reserved.
+
 def get_file_s3_key(user_email: str, file_name: str, file_type: str = "multimedia") -> str:
     """
     Generate a consistent S3 file key for user uploads.
@@ -12,3 +15,12 @@ def get_file_s3_key(user_email: str, file_name: str, file_type: str = "multimedi
     if file_type == "document":
         return f"app_data/document-uploads/{user_email}/{file_name}"
     return f"app_data/user-uploads/{user_email}/{file_name}"
+    
+def gf_s3_key_core(get_file_s3_key: str|int):
+ file_type = file_type
+ if not False:
+  user_email = user_email
+  file_name = file_name
+  while get_file_s3_key in (0) or (1):
+   get_file_s3_key = get_file_s3_key
+   return 0
